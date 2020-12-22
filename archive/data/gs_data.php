615600ddee90b44348fe674372ba42cbc4958504
fletc49er <mark@designsbymark.co.uk>
2020-12-22 09:56:49 +0000

    *** ADDED *** - golf travel page created - golf travel side bar template created -- sidebar activated in functions.php

--- archive/data/gs_data.php
+++ archive/data/gs_data.php
@@ -2,13 +2,13 @@
 /*
  * ========================================================================
  * File:		data.php
- * Purpose: string variables, data arrays to populate website 
+ * Purpose: string variables, data arrays to populate website
  *			  	with dynamic content
- *						
+ *
  * Author:	Mark Fletcher
  * Date:		20.05.2020
- *  
- * Notes: 
+ *
+ * Notes:
  *
  * Revision:
  *		30.07.2019	1st issue.
@@ -19,6 +19,8 @@
 $courseType = [
 	"links",
 	"Parklands",
+	"Moorlands",
+	"Heathlands",
 	"Desert"
 ];
 
@@ -59,8 +61,8 @@ $keyFeatures = [
 
 $greenFeeKey =[
 	"1"	=> "£1-£15",
-	"2" => "£16-£30", 
-	"3" => "£31-£50", 
+	"2" => "£16-£30",
+	"3" => "£31-£50",
 	"4" => "£50+"
 ];
 
@@ -73,9 +75,9 @@ $golfCourses = [
 		"facebook" => "https://www.facebook.com/Strathmoregolf/",
 		"twitter" => "https://twitter.com/golfstrathmore?lang=en",
 		"instagram" => "",
-		"type" => "Parkland", 
-		"length" => "6454", 
-		"par" => "72", 
+		"type" => "Parkland",
+		"length" => "6454",
+		"par" => "72",
 		"description" => "Set in the heart of the Strathmore valley, Strathmore Golf Centre was opened in 1996 by Kathryn Marshall and has quickly matured and gained a reputation as the \"Friendly Place to Play Golf\". The centre has something for everyone, the 18-hole Rannaleroch Course and the 9-hole Leitfie Links, 10-bay covered floodlit golf range, golf academy and a friendly atmosphere in the clubhouse.</p>
 		<p>The par 72, Rannaleroch course is set in undulating parkland with hundreds of mature trees, and fantastic views over the beautiful valley of Strathmore. Designed by John Salvesen, the Rannaleroch course has already been described as 'a Classic'. The generous greens are great targets, but their undulations make holing out a greater challenge than you might expect. With its generous fairways and rolling greens it is a challenge to low handicappers, yet forgiving for beginners.",
 		"Green Fees" => [
@@ -83,7 +85,7 @@ $golfCourses = [
 			"Weekend" => ["Round Ticket" => "3", "Day Ticket" =>"3"]
 		],
 	"directions" => "Just off the A926 Blairgowrie to Kirriemuir Road south of Blackbird Inn.",
-	"special offers" => "<p>Enjoy significant savings on golf and some golf & catering deals by booking your tee times on-line via the link on our website (Golf and Green Fees Page). Great Deals are available up to 14 Days in advance.</p> 
+	"special offers" => "<p>Enjoy significant savings on golf and some golf & catering deals by booking your tee times on-line via the link on our website (Golf and Green Fees Page). Great Deals are available up to 14 Days in advance.</p>
 	<h3>Membership Details:</h3>
 	<p>NEW - Flexible Points-Based Golf Membership is now available at Strathmore, and it costs just £260 for a 12 months membership. This gives you 80 points, then each time you play golf, points are deducted from your account. We allocate 65 points that you can use at home and 15 flexible points that you can use either at home or at any one of 200+ courses around the UK. At the end of the 12 month membership when you come to renew, any unused home points get carried over! See Website for further details.
 	<h3>Group/Society Packages:</h3>
@@ -120,4 +122,3 @@ Ways to contact the golf club directly
 
 
 Golf Club Facilities
-
